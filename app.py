24a839f30272657880052e68068dccabeb11085b
gezy369 <107927231+gezy369@users.noreply.github.com>
2026-01-26 15:03:16 +0100

    -

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello world!"
+
+    import csv
+
+    with open("./imported_data/Orders.csv", newline="") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            print(row)
