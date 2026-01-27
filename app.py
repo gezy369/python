18997dc7a986c001193b38f4e9b6c4d40e5aed7a
gezy369 <107927231+gezy369@users.noreply.github.com>
2026-01-27 07:29:33 +0100

    -

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
+import pandas as pd
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello world!"
+@app.route("/data")
+def data():
+    csv_path = os.path.join("imported_data", "Orders.csv")
+    df = pd.read_csv(csv_path)
+    return jsonify(df.to_dict(orient="records"))
