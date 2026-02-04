b8b0a4d3d1f664e4e3c13624db23742e472b493d
Greg <sir.gezy@gmail.com>
2026-02-04 19:22:24 +0100

    -

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from werkzeug.utils import secure_filename
 from datetime import datetime
-from utils.functions import csv_handler, plot_trade_candlestick
+from utils.functions import csv_handler
 import os
 import io
 import pandas as pd
